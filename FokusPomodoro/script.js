e2a44507d520284a72f617237da3fa0ce7306944
Bruno Rasera <bruno.ras07@gmail.com>
2024-05-14 17:03:41 -0300

    Added task list

--- FokusPomodoro/script.js
+++ FokusPomodoro/script.js
@@ -10,11 +10,10 @@ const startPauseBt = document.querySelector('#start-pause');
 const iniciarOuPausarBt = document.querySelector('#start-pause span');
 const iniciarOuPausarBtIcone = document.querySelector('.app__card-primary-butto-icon');
 const tempoNaTela = document.querySelector('#timer');
-const musica = new Audio('/sons/luna-rise-part-one.mp3');
-const audioPlay = new Audio('/sons/play.wav');
-const audioPausa = new Audio('/sons/pause.mp3');
+const musica = new Audio('./sons/luna-rise-part-one.mp3');
+const audioPlay = new Audio('./sons/play.wav');
+const audioPausa = new Audio('./sons/pause.mp3');
 const audioTempoFinalizado = new Audio('./sons/beep.mp3')
-
 
 let tempoDecorridoEmSegundos = 1500;
 let intervaloId = null;
@@ -36,7 +35,7 @@ function alterarContexto(contexto) {
     contexto.classList.remove('active');
   })
   html.setAttribute('data-contexto',contexto);
-  banner.setAttribute('src', `/imagens/${contexto}.png`);
+  banner.setAttribute('src', `./imagens/${contexto}.png`);
   switch (contexto) {
     case "foco":
       titulo.innerHTML = `Otimize sua produtividade,<br>
@@ -76,6 +75,11 @@ longoBt.addEventListener('click', () => {
 const contagemRegressiva = () => {
   if (tempoDecorridoEmSegundos <= 0) {
     audioTempoFinalizado.play();
+    const focoAtivo = html.getAttribute('data-contexto') == 'foco';
+    if (focoAtivo) {
+      const evento = new CustomEvent('FocoFinalizado');
+      document.dispatchEvent(evento);
+    }
     alert('Tempo finalizado!');
     zerar();
     return;
@@ -95,13 +99,13 @@ function iniciarOuPausar() {
   audioPlay.play();
   intervaloId = setInterval(contagemRegressiva, 1000);
   iniciarOuPausarBt.textContent = "Pausar";
-  iniciarOuPausarBtIcone.setAttribute('src', '/imagens/pause.png');
+  iniciarOuPausarBtIcone.setAttribute('src', './imagens/pause.png');
 }
 
 function zerar() {
   clearInterval(intervaloId);
   iniciarOuPausarBt.textContent = "Começar"
-  iniciarOuPausarBtIcone.setAttribute('src', '/imagens/play_arrow.png');
+  iniciarOuPausarBtIcone.setAttribute('src', './imagens/play_arrow.png');
   intervaloId = null;
 }
 
